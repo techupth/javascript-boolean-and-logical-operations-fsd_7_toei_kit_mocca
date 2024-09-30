57d6457fbb9fe0b1a2c6c1ef145739f8c5e6a2ed
HaruDev <sarankhtn@gmail.com>
2024-09-30 15:25:06 +0700

    feat : add all assignments

--- ex-1.js
+++ ex-1.js
@@ -6,3 +6,9 @@ let isAllow;
 // Start coding here
 
 console.log(isAllow);
+isOver18 = true;
+hasCriminalBlacklist = false;
+isAllow = isOver18 && !hasCriminalBlacklist;
+console.log(isAllow);
+
+
--- ex-2.js
+++ ex-2.js
@@ -10,3 +10,12 @@ let isPlatinum;
 let hasPromotion;
 
 console.log(hasPromotion);
+
+// John's qualification
+lastMonthPaidMoreThan4000 = true;
+isWeekday = true;
+hasBoughtProductFromITCategory = false;
+hasAttendedDiscountEvent = true;
+isPlatinum = false;
+hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent) || isPlatinum;
+console.log(hasPromotion);
